283a7ff5da747be06bfe6b61a7d0e3a60edbaf59
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-04-04 13:09:51 +0200

    klar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Header from './components/Header';
 import Product from './components/Product';
 
 function App() {
+  const products = [
+    { title: "A Sign of Four", author: "Av Sir Arthur Conan Doyle", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+    { title: "A Study in Scarlet", author: "Av Sir Arthur Conan Doyle", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+    { title: "Baskervilles Hound", author: "Av Sir Arthur Conan Doyle", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+    { title: "The Adventure of Sherlock Holmes", author: "Av Sir Arthur Conan Doyle", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+    ];
+
+
+    const ProductItem = products.map ((product) => {
+        return  <Product title = {product.title} author = {product.author} description= {product.description} />
+    });
+    
   return (
     <div className="App">
       <Header />
       <section className='cards'>
-            <Product title="A Sign of Four" author="Av Sir Arthur Conan Doyle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."/>
-            <Product title="A Study in Scarlet" author="Av Sir Arthur Conan Doyle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
-            <Product title="Baskervilles Hound" author="Av Sir Arthur Conan Doyle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
-            <Product title="The Adventure of Sherlock Holmes" author="Av Sir Arthur Conan Doyle" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "/>
+            {ProductItem}
         </section>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,16 @@
 import './Product.css'
 
 function Product (props) {
-    const {title, author, description} = props;
+   const {title, author, description} = props;
+  
+
+
     return (
         <article className='card'>
-            <h2 className='title'> { title }</h2>
-            <h3 className='author'> { author }</h3>
-            <p className='description'> { description }</p>
-            <button className='add-button'>Add to cart</button>
+            <h2 className='title'> {title}</h2>
+            <h3 className='author'> {author} </h3>
+            <p className='description'> {description} </p>
+            <button className='add-button'>  </button>
         </article>
     )
 }
